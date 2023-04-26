f00912dde84d21fc7b9a34f0caaa8d988de29a80
Abraham LC <alc_inf@yahoo.com.mx>
2023-04-25 18:11:25 -0600

    Class 1.5. intro blade

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Home';
-});
-
-Route::prefix('/company')->group(function () {
-    
-    Route::get('/about_us/{id}', function ($id) {
-        return 'About us: '.$id;
-        //return request()->segment(2); //Obtener segmento del url
-    });
-
-    Route::get('/contact', function () {
-        return 'Contact';
-    });
-
+    return view('welcome');
 });
